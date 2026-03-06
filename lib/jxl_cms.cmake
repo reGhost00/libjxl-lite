a3ad2e0b5991880027b6fe59ddb0058b7a8f9b9e
reghost <reghost.lyc@gmail.com>
2026-03-06 13:03:39 +0800

    移除无用组件，精简构建系统

--- lib/jxl_cms.cmake
+++ lib/jxl_cms.cmake
@@ -30,15 +30,7 @@ target_include_directories(jxl_cms BEFORE PUBLIC
 
 set(JPEGXL_CMS_LIBRARY_REQUIRES "")
 
-if (JPEGXL_ENABLE_SKCMS)
-  target_link_skcms(jxl_cms)
-else()
-  target_link_libraries(jxl_cms PRIVATE lcms2)
-  if (JPEGXL_FORCE_SYSTEM_LCMS2)
-    set(JPEGXL_CMS_LIBRARY_REQUIRES "lcms2")
-  endif()
-endif()
-
+target_link_skcms(jxl_cms)
 target_link_libraries(jxl_cms PRIVATE hwy)
 
 set_target_properties(jxl_cms PROPERTIES
@@ -46,37 +38,22 @@ set_target_properties(jxl_cms PROPERTIES
         SOVERSION ${JPEGXL_LIBRARY_SOVERSION})
 
 # Check whether the linker support excluding libs
-if (MSVC)
-  # MSVC ignores this flag (with a warning), so CMake thinks it supports that.
-  set(LINKER_EXCLUDE_LIBS_FLAG "")
-  set(LINKER_SUPPORT_EXCLUDE_LIBS FALSE)
-else()
-  set(LINKER_EXCLUDE_LIBS_FLAG "-Wl,--exclude-libs=ALL")
-  include(CheckCSourceCompiles)
-  list(APPEND CMAKE_REQUIRED_LINK_OPTIONS ${LINKER_EXCLUDE_LIBS_FLAG})
-  check_c_source_compiles("int main(){return 0;}" LINKER_SUPPORT_EXCLUDE_LIBS)
-  list(REMOVE_ITEM CMAKE_REQUIRED_LINK_OPTIONS ${LINKER_EXCLUDE_LIBS_FLAG})
-endif()
+set(LINKER_EXCLUDE_LIBS_FLAG "-Wl,--exclude-libs=ALL")
+include(CheckCSourceCompiles)
+list(APPEND CMAKE_REQUIRED_LINK_OPTIONS ${LINKER_EXCLUDE_LIBS_FLAG})
+check_c_source_compiles("int main(){return 0;}" LINKER_SUPPORT_EXCLUDE_LIBS)
+list(REMOVE_ITEM CMAKE_REQUIRED_LINK_OPTIONS ${LINKER_EXCLUDE_LIBS_FLAG})
 
 if(LINKER_SUPPORT_EXCLUDE_LIBS)
   set_property(TARGET jxl_cms APPEND_STRING PROPERTY
       LINK_FLAGS " ${LINKER_EXCLUDE_LIBS_FLAG}")
 endif()
 
-install(TARGETS jxl_cms
-        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
-
 if (BUILD_SHARED_LIBS)
   set(JPEGXL_REQUIRES_TYPE "Requires.private")
-  set(JPEGXL_CMS_PRIVATE_LIBS "-lm ${PKGCONFIG_CXX_LIB}")
 else()
   set(JPEGXL_REQUIRES_TYPE "Requires")
-  set(JPEGXL_CMS_PRIVATE_LIBS "-lm ${PKGCONFIG_CXX_LIB}")
 endif()
+set(JPEGXL_CMS_PRIVATE_LIBS "-lm ${PKGCONFIG_CXX_LIB}")
 
-configure_file("${CMAKE_CURRENT_SOURCE_DIR}/jxl/libjxl_cms.pc.in"
-               "libjxl_cms.pc" @ONLY)
-install(FILES "${CMAKE_CURRENT_BINARY_DIR}/libjxl_cms.pc"
-  DESTINATION "${CMAKE_INSTALL_LIBDIR}/pkgconfig")
+configure_file("${CMAKE_CURRENT_SOURCE_DIR}/jxl/libjxl_cms.pc.in" "libjxl_cms.pc" @ONLY)
